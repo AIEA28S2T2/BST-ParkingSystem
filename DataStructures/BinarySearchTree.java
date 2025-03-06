2e64143b7bd22abc0bff3999442cf38d25e24d5c
Niranjan S <niru2169@gmail.com>
2025-03-06 12:13:21 +0530

    added unimplemented methods

--- DataStructures/BinarySearchTree.java
+++ DataStructures/BinarySearchTree.java
@@ -1,5 +1,23 @@
 package DataStructures;
 
 public class BinarySearchTree extends Tree {
+
+    @Override
+    protected Node searchForFreeSpot() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'searchForFreeSpot'");
+    }
+
+    @Override
+    protected Node removeSpot(Node node) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeSpot'");
+    }
+
+    @Override
+    protected void insertSpot(Node node) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insertSpot'");
+    }
     
 }

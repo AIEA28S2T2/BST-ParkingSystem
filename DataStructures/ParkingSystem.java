25f05026af0231b4a19346ba1704a92dd1f086e0
Niranjan S <niru2169@gmail.com>
2025-03-06 18:03:13 +0530

    added more comments

--- DataStructures/ParkingSystem.java
+++ DataStructures/ParkingSystem.java
@@ -1,5 +1,22 @@
 package DataStructures;
 
+/*
+In the constructor, get int n as input and create the new tree upon initialization
+
+Create the following methods here:
+ 1. add a car to the parking lot:
+    find a free spot(spotID)
+    add the node into the queue
+    get input for regNo and make changes to the node
+    
+2. remove a car from the parking lot:
+    get input for regNo
+    search for the node with the regNo
+    remove the node from the queue
+    add the node back to the tree
+    
+ */
+
 public class ParkingSystem {
     
 }
